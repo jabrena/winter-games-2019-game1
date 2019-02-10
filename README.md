7f460d48b5beaf7106b41a68bcb96830f3ee7532
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-02-10 16:19:24 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # winter-games-2019-game1
 Implementation of first challenge
+
+```
+./mvnw clean test
+./mvnw clean site
+./mvnw surefire-report:report 
+```
---
+++ src/main/java/org/jab/microservices/wintergames1/WinterGames1.java
@@ -0,0 +1,14 @@
+package org.jab.microservices.wintergames1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterGames1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterGames1.class, args);
+	}
+
+}
+
