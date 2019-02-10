25290730c9410249c207276986923ed48828b135
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-02-10 17:10:07 +0100

    Adding the first Test

---
+++ src/test/java/org/jab/microservices/wintergames1/IntegrationTests.java
@@ -0,0 +1,30 @@
+package org.jab.microservices.wintergames1;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void exampleTest() throws Exception {
+        this.mvc.perform(get("/api/version")).andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+}
