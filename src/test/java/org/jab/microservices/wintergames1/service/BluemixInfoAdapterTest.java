e4d154881b7404cd52e60596b46ff2f70570684e
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-03-17 21:01:16 +0100

    Increasing timeout

--- src/test/java/org/jab/microservices/wintergames1/service/BluemixInfoAdapterTest.java
+++ src/test/java/org/jab/microservices/wintergames1/service/BluemixInfoAdapterTest.java
@@ -62,7 +62,7 @@ public class BluemixInfoAdapterTest {
 
         final String response = getResourceAsString("bluemix_info_success_response.json");
         stubFor(get(urlEqualTo("/v2/info"))
-                .willReturn(okJson(response).withFixedDelay(5000)));
+                .willReturn(okJson(response).withFixedDelay(15000)));
 
         StepVerifier.create(bluemixInfoAdapter.getVersion())
                 .expectNext(false)
--- src/test/java/org/jab/microservices/wintergames1/service/PCFInfoAdapterTest.java
+++ src/test/java/org/jab/microservices/wintergames1/service/PCFInfoAdapterTest.java
@@ -62,7 +62,7 @@ public class PCFInfoAdapterTest {
 
         final String response = getResourceAsString("bluemix_info_success_response.json");
         stubFor(get(urlEqualTo("/v2/info"))
-                .willReturn(okJson(response).withFixedDelay(5000)));
+                .willReturn(okJson(response).withFixedDelay(15000)));
 
         StepVerifier.create(pcfInfoAdapter.getVersion())
                 .expectNext(false)
