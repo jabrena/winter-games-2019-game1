c49277cea4fe6a3bd48e4cfb9e5153c80cca19ce
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-03-17 20:13:47 +0100

    Fixing issues for the unit tests for the Handler

--- src/test/java/org/jab/microservices/wintergames1/WinterGames1Tests.java
+++ src/test/java/org/jab/microservices/wintergames1/WinterGames1Tests.java
@@ -2,11 +2,11 @@ package org.jab.microservices.wintergames1;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@WebFluxTest
 public class WinterGames1Tests {
 
 	@Test
--- src/test/java/org/jab/microservices/wintergames1/handler/MyHandlerTest.java
+++ src/test/java/org/jab/microservices/wintergames1/handler/MyHandlerTest.java
@@ -1,13 +1,14 @@
 package org.jab.microservices.wintergames1.handler;
 
+import org.jab.microservices.wintergames1.config.WebConfig;
 import org.jab.microservices.wintergames1.service.BluemixInfoAdapter;
 import org.jab.microservices.wintergames1.service.PCFInfoAdapter;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -15,19 +16,19 @@ import reactor.test.StepVerifier;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-class MyHandlerTest {
+@WebFluxTest({MyHandler.class, WebConfig.class})
+@DirtiesContext
+public class MyHandlerTest {
 
-    @MockBean(name = "pcf")
+    @MockBean
     private PCFInfoAdapter pcfInfoAdapter;
 
-    @MockBean(name = "bluemix")
+    @MockBean
     private BluemixInfoAdapter bluemixInfoAdapter;
 
     @Autowired
     private MyHandler myHandler;
 
-    @Disabled
     @Test
     public void Given_a_request_When_check_version_in_parallel_And_both_version_are_Ok_Then_returns_true() {
 
@@ -42,7 +43,6 @@ class MyHandlerTest {
                 .verify();
     }
 
-    @Disabled
     @Test
     public void Given_a_request_When_check_version_in_parallel_And_any_version_is_not_Ok_Then_returns_false() {
 
@@ -57,7 +57,6 @@ class MyHandlerTest {
                 .verify();
     }
 
-    @Disabled
     @Test
     public void Given_a_request_When_check_version_in_sequence_And_both_version_are_Ok_Then_returns_true() {
 
@@ -72,7 +71,6 @@ class MyHandlerTest {
                 .verify();
     }
 
-    @Disabled
     @Test
     public void Given_a_request_When_check_version_in_sequence_And_any_version_is_not_Ok_Then_returns_false() {
 
